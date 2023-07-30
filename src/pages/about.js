cf577fdd00d826064af3279ca0456f920a3b18eb
prakhar040 <prakhar.srivastava040@gmail.com>
2023-07-30 15:06:48 +0530

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      Hello About! How are you
+    </div>
+  )
+}
+
+export default about
---
+++ src/pages/blogpost/how_to_use_keyboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const How = () => {
+  return (
+    <div>
+      How to use Keyboard
+    </div>
+  )
+}
+
+export default How
